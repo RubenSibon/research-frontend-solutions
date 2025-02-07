0f1ef5d61844ed5aa0a93ebf19c20b1cce6c1204
Ruben Sibon <r.sibon@amsterdam.nl>
2025-02-07 15:01:38 +0100

    build: upgrade to React 19

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -1,13 +1,7 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import "./app.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
